c6aebdfb6e5ed6f1730d6d25774ffde2fa4a261a
tranhung26092002 <hungtran0168652718@gmail.com>
2025-04-24 20:47:49 +0700

    s

--- backend-nestjs/src/auth/auth.service.ts
+++ backend-nestjs/src/auth/auth.service.ts
@@ -54,10 +54,10 @@ export class AuthService {
             }
 
             // Attendance
-            // const date = new Date().toLocaleString("vi-VN", {
-            //     timeZone: "Asia/Ho_Chi_Minh",
-            // });
-            const date = "15:43:10 23/4/2025"
+            const date = new Date().toLocaleString("vi-VN", {
+                timeZone: "Asia/Ho_Chi_Minh",
+            });
+            // const date = "15:43:10 23/4/2025"
             const [timePart, datePart] = date.split(" ");
             const [hours, minutes] = timePart.split(":");
             console.log(date);
